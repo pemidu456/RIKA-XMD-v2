c36677b15f2e4d30d2593d1d31c2f0f01bc153d4
pemidu456 <pemidumabudashita@gmail.com>
2025-07-19 22:02:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "RIKA-XMD=YOUR-SESSION", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "RIKA-XMD=5JI3WTzT#_KdmdntxIgNbOb0DHpzSpsEEceVCRFj61fx0VBQPwWY", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
